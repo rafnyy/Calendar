f9ab7ed20b758bbffb8010ef928f6510b560e6bb
Rob <fuscorob@gmail.com>
2015-11-12 11:43:49 -0800

    Use NamedParameterStatement for clarity and safety, no exception on insert

--- src/main/java/rest/Book.java
+++ src/main/java/rest/Book.java
@@ -1,22 +1,27 @@
 package rest;
 
+import cal.Constants;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import database.DatabaseConnection;
+import database.NamedParameterStatement;
+import org.joda.time.Instant;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
+import java.sql.Timestamp;
 
 @Path("/book")
 @Singleton
 public class Book {
+    private static final String CLIENT_ID = "client_id";
+    private static final String CONSULTANT_ID = "consultant_id";
+    private static final String TO_STATUS = "to_status";
+    private static final String START = "start";
+    private static final String DURATION_MS = "duration_ms";
     private final DatabaseConnection connection;
 
     @Inject
@@ -28,19 +33,25 @@ public class Book {
     @Consumes(MediaType.APPLICATION_JSON)
     public void book() {
 
+        Instant instant = new Instant();
 
         try
         {
-            Statement st = connection.getConnection().createStatement();
-            st.executeQuery("INSERT INTO \"Schedule\"(\n" +
-                    "            client_id, consultant_id, to_status, start)\n" +
-                    "    VALUES (2, 3, 'BOOKED', NOW());\n");
+            String insertTableSQL = "INSERT INTO \"Schedule\""
+                + "(" + CLIENT_ID + ", " + CONSULTANT_ID + ", " + TO_STATUS + ", " + START + ", " + DURATION_MS + ") VALUES"
+                + "(:" + CLIENT_ID + ", :" + CONSULTANT_ID + ", :" + TO_STATUS + ", :" + START + ", :" + DURATION_MS + ")";
 
+            NamedParameterStatement preparedStatement = new NamedParameterStatement(connection.getConnection(), insertTableSQL);
+            preparedStatement.setInt(CLIENT_ID, 4);
+            preparedStatement.setInt(CONSULTANT_ID, 4);
+            preparedStatement.setString(TO_STATUS, "BOOKED");
+            preparedStatement.setTimestamp(START, new Timestamp(instant.getMillis()));
+            preparedStatement.setInt(DURATION_MS, Constants.millisecondsInAnHour);
 
-            st.close();
+            preparedStatement .executeUpdate();
         }
         catch (SQLException se) {
-            System.err.println("Threw a SQLException creating the list of blogs.");
+            System.err.println("Threw a SQLException booking an appointment.");
             System.err.println(se.getMessage());
         }
     }
